a58705ea08234eb88d10ae9e55a265a41d62ed74
TAMU73 <47109205+TAMU73@users.noreply.github.com>
2020-05-21 18:36:26 +0545

    Setting Stream Provider in Wrapper widget

--- lib/screen/wrapper.dart
+++ lib/screen/wrapper.dart
@@ -1,11 +1,16 @@
+import 'package:darpandentalhome/model/user.dart';
 import 'package:darpandentalhome/screen/authenticate/authenticate.dart';
 import 'package:darpandentalhome/screen/home/home_page.dart';
 import 'package:darpandentalhome/screen/payment/payment.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+
     return Authenticate();
   }
 }
