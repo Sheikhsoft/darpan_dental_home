70093f818d76874cbecfd08c6e689e72fae82b40
TAMU73 <47109205+TAMU73@users.noreply.github.com>
2020-05-21 18:31:30 +0545

    Stream Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:darpandentalhome/screen/wrapper.dart';
+import 'package:darpandentalhome/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MaterialApp(
   home: Main(),
@@ -12,12 +14,15 @@ class Main extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<OverscrollIndicatorNotification>(
-      onNotification: (OverscrollIndicatorNotification overScroll) {
-        overScroll.disallowGlow();
-        return false;
-      },
-      child: Wrapper(),
+    return StreamProvider.value(
+      value: AuthService().user,
+      child: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (OverscrollIndicatorNotification overScroll) {
+          overScroll.disallowGlow();
+          return false;
+        },
+        child: Wrapper(),
+      ),
     );
   }
 }
