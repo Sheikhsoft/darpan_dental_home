dead06690bf59b4e57efde0269a7e90232753780
TAMU73 <47109205+TAMU73@users.noreply.github.com>
2020-06-02 17:36:42 +0545

    PDF file worked in release version

--- lib/screen/wrapper.dart
+++ lib/screen/wrapper.dart
@@ -1,7 +1,6 @@
 import 'package:darpandentalhome/model/user.dart';
 import 'package:darpandentalhome/screen/authenticate/authenticate.dart';
 import 'package:darpandentalhome/screen/home/home_page.dart';
-import 'package:darpandentalhome/screen/payment/payment.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
